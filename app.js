ef1250c417fcaecaeaaf45180faf23c75a3f1f3d
Marta <martagarciamgo@gmail.com>
2023-12-10 23:25:36 +0100

    añado middleware de manejo de errores y parseo json

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 
+//Importamos las rutas.
+import postRoutes from './src/routes/postRoutes.js';
+
 // Creamos un servidor con express.
 const app = express();
+
+//Middleware que parsea un body en formato JSON.
+app.use(express.json());
 
 // Middleware que muestre por consola el método y la ruta (endpoint) de la petición entrante.
 app.use(morgan('dev'));
 
+//Esta función queda sustituida por la anterior de morgan.
 //app.use((req, res, next) => {
 //  console.log(`${req.method} ${req.url}`);
 //
@@ -18,19 +25,33 @@ app.use(morgan('dev'));
 //  next();
 //});
 
+//Middleware que indica a express dónde están las rutas.
+app.use(postRoutes);
+
+//Los dos middleware siguientes son sustituidos por el middleware de arriba que indica a express dónde están las rutas.
 // Middleware que me devuelve todos los post de la base de datos.
-app.get('/posts', (req, res) => {
-    res.send({
-        status: 'ok',
-        message: 'Aquí tienes el listado de posts',
-    });
-});
+//app.get('/posts', (req, res) => {
+//  res.send({
+//    status: 'ok',
+//  message: 'Aquí tienes el listado de posts',
+//});
+//});
 
 // Middleware que crea un post en la base de datos.
-app.post('/posts', (req, res) => {
-    res.status(201).send({
-        status: 'ok',
-        message: 'Post creado',
+//app.post('/posts', (req, res) => {
+//  res.status(201).send({
+//    status: 'ok',
+//  message: 'Post creado',
+//});
+//});
+
+//Middleware de manejo de errores.
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.httpStatus || 500).send({
+        status: 'error',
+        message: err.message,
     });
 });
 
