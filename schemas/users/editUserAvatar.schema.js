693bc0d7ad1cfb00f84728d09676fc69156c59cd
Danie204 <142260338+Danie204@users.noreply.github.com>
2023-12-13 18:17:34 +0100

    Delete schemas/users/editUserAvatar.schema.js

--- schemas/users/editUserAvatar.schema.js
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-import imgSchema from '../imgSchema.js';
-
-const editUserAvatarSchema = joi.object({
-    avatar: imgSchema.required()
-});
-
-export default editUserAvatarSchema;
