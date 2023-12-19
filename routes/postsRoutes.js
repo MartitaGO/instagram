4d384f98da537b2ffa7c6235bea2afd7b413516d
Julen Ortega <142092974+JulenOrtega@users.noreply.github.com>
2023-12-19 11:31:39 +0100

    Delete routes directory

--- routes/postsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import lookPhotos from '../middlewares/lookPhoto.middleware.js';
-
-const router = express.Router();
-
-router.get('/photos/look', lookPhotos)
-
-export default router;
